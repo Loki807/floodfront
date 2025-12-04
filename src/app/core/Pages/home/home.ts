7e12c20476057c6e42dc554ca1b6cd084b723919
sajikaran <kanna20021130@gmail.com>
2025-12-04 13:43:00 +0530

    chages

--- src/app/core/Pages/home/home.ts
+++ src/app/core/Pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Request } from '../../services/request';
 @Component({
   selector: 'app-home',
   imports: [],
@@ -8,9 +8,27 @@ import { Router } from '@angular/router';
   styleUrl: './home.css',
 })
 export class Home {
-  currentYear = new Date().getFullYear();
+ currentYear = new Date().getFullYear();
 
-  constructor(private router: Router) {}
+  urgencyCounts = { High: 0, Medium: 0, Low: 0 };
+
+  constructor(
+    private router: Router,
+    private requestService: Request
+  ) {}
+
+  ngOnInit() {
+    this.loadUrgencyCounts();
+  }
+
+  loadUrgencyCounts() {
+    this.requestService.getAllRequests().subscribe(res => {
+      this.urgencyCounts.High = res.filter(r => r.urgency === 'High').length;
+      this.urgencyCounts.Medium = res.filter(r => r.urgency === 'Medium').length;
+      this.urgencyCounts.Low = res.filter(r => r.urgency === 'Low').length;
+    });
+  }
+  
 
   goToSubmit() {
     this.router.navigate(['/submit']);
@@ -20,9 +38,13 @@ export class Home {
     this.router.navigate(['/dashboard11']);
   }
 
-  
   Privacy() {
     this.router.navigate(['/privacy']);
+  }
+
+
+   goToUrgency(level: string) {
+    this.router.navigate(['/filter/urgency', level]);
   }
 
 }
